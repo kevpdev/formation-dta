cfb0357ac4811526c545c4c0187f2287944c34a9
kevdev <prepontkevin@gmail.com>
2016-12-05 12:54:06 +0100

    tp3 project multi module

---
+++ workspace/dta-pizzeria/dta-pizzeria-model/src/main/java/dta_pizzeria/dta_pizzeria_model/App.java
@@ -0,0 +1,13 @@
+package dta_pizzeria.dta_pizzeria_model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ workspace/dta-pizzeria/src/main/java/dta_pizzeria/dta_pizzeria/App.java
@@ -0,0 +1,13 @@
+package dta_pizzeria.dta_pizzeria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
