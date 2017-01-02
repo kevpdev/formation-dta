163a7b99739e2f135c49f5d620b591dff12708b0
kevdev <prepontkevin@gmail.com>
2017-01-02 11:36:01 +0100

    modif branch

---
+++ kevin-dta-pizzeria/kevin-pizzeria-web/src/main/java/fr/pizzeria/admin/service/ApplicationConfig.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package fr.pizzeria.admin.service;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * @author ETY11
+ * @since 2 janv. 2017
+ */
+public class ApplicationConfig extends Application {
+
+	/**
+	 * Constructeur de la classe 
+	 * @date 2 janv. 2017
+	 * @author ETY11
+	 */
+	public ApplicationConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
